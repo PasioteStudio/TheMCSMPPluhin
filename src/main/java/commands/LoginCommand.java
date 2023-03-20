6e30bb5e99ff6d6a9eb9501a25371349b9bf3373
Acsilaa <galeengames@gmail.com>
2023-03-20 15:39:08 +0100

    started login

--- src/main/java/commands/LoginCommand.java
+++ src/main/java/commands/LoginCommand.java
@@ -1,4 +1,28 @@
 package commands;
 
-public class LoginCommand {
+import models.PlayerNote;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import utils.NoteStorageUtil;
+
+public class LoginCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player)) return false;
+        if(args.length != 1) return false;
+        Player player = ((Player) sender).getPlayer();
+        PlayerNote playerNote = NoteStorageUtil.ReadPlayerNote(player);
+
+        String input = args[0];
+        String hashed = playerNote.hashed_passw;
+        if(input != hashed){
+
+            return true;
+        }
+
+
+        return true;
+    }
 }
--- src/main/java/me/plugout/PlugOut.java
+++ src/main/java/me/plugout/PlugOut.java
@@ -1,8 +1,10 @@
 package me.plugout;
 
+import commands.LoginCommand;
 import commands.RegisterCommand;
 import commands.SetGlobalSpawn;
 import commands.SetGlobalSpawnCompleter;
+import models.PlayerNote;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Sound;
@@ -17,11 +19,13 @@ import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.configuration.Configuration;
+import utils.NoteStorageUtil;
 
 import java.io.File;
 
@@ -47,6 +51,7 @@ public final class PlugOut extends JavaPlugin implements Listener {
         getCommand("register").setExecutor(new RegisterCommand());
         getCommand("setglobalspawn").setExecutor(new SetGlobalSpawn(this));
         getCommand("setglobalspawn").setTabCompleter(new SetGlobalSpawnCompleter());
+        getCommand("login").setExecutor(new LoginCommand());
     }
 
     @Override
@@ -116,6 +121,24 @@ public final class PlugOut extends JavaPlugin implements Listener {
     public void OnPlayerQuit(PlayerQuitEvent pqe){
         Player _player = pqe.getPlayer();
         pqe.setQuitMessage(ChatColor.RED + "-- " + _player.getDisplayName());
+        FileConfiguration conf = getConfig();
+
+        if(_player.getWorld() != getServer().getWorld(String.valueOf(conf.get("playWorlds")))) return;
+        PlayerNote playerNote = NoteStorageUtil.ReadPlayerNote(_player);
+        if(playerNote == null) {
+            throw new RuntimeException();
+        }
+        Location loc = _player.getLocation();
+        playerNote.playWorldPos = new double[]{loc.getX(), loc.getY(), loc.getZ()};
+        Inventory inv = _player.getInventory();
+        playerNote.playWorldInv = inv.getContents();
+
+        try {
+            NoteStorageUtil.SavePlayerNote(playerNote);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
